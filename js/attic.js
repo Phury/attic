9140789fe00c6d8826a846ce4d71c416b78de444
Phury <phvdvelde@gmail.com>
2017-11-03 13:17:19 +0100

    use yeti theme

--- js/attic.js
+++ js/attic.js
@@ -31,7 +31,7 @@ class NavigationComponent extends React.Component {
     
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg fixed-top bg-white">
         <a className="navbar-brand" href="#">Attic</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -65,7 +65,7 @@ class NavigationComponent extends React.Component {
           </ul>
           <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>
       </nav>
